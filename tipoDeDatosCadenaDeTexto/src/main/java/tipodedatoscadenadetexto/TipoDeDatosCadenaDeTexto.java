27a34eb1a689d4768c6fd00763ba78550d20b981
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-08-23 07:39:25 -0500

    video string

---
+++ tipoDeDatosCadenaDeTexto/src/main/java/tipodedatoscadenadetexto/TipoDeDatosCadenaDeTexto.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package tipodedatoscadenadetexto;
+
+/**
+ *
+ * @author juan bonilla
+ */
+public class TipoDeDatosCadenaDeTexto {
+
+    public static void main(String[] args) {
+      
+        
+         String saludo= "hola mundo"; // una varibale tipo string permite 
+         int longitud = saludo.length(); //con esto podemos saber la longitud del string
+         boolean stringVacio = saludo.isEmpty(); // determina si e string esta vacio
+         
+         char myChar = saludo.charAt(2);// funcion para tomar la iniciales de un string
+         
+         
+        
+         
+    }
+}
